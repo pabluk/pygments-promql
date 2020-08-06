6e2e8426354eded69a3f4357c2cda9887db7109d
Pablo SEMINARIO <pablo@seminar.io>
2020-08-06 18:00:44 +0200

    Bumped to version 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygments-promql",
-    version="0.0.4",
+    version="0.0.5",
     author="Pablo Seminario",
     author_email="pablo@seminar.io",
     description="A PromQL lexer for Pygments",
